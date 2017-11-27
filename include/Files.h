00dab435482e4f3ddedbe5371998cfc66f35ec6e
Alexander Freyr Lúðvíksson <alex98freyr@gmail.com>
2017-11-27 13:22:16 +0000

    Rest of the classes

---
+++ include/Files.h
@@ -0,0 +1,20 @@
+#ifndef FILES_H
+#define FILES_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+/// This class is for using file streams
+/// to read and write files
+class Files
+{
+private:
+    string orders;
+    string finishedOrders;
+    string menu;
+public:
+
+};
+
+#endif // FILES_H
---
+++ include/UI.h
@@ -0,0 +1,16 @@
+#ifndef UI_H
+#define UI_H
+
+
+class UI
+{
+    public:
+        UI();
+        virtual ~UI();
+
+    protected:
+
+    private:
+};
+
+#endif // UI_H
