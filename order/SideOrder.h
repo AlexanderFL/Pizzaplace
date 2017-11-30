42eaf8ed3f28bd28d150d875fb170e100e28cc54
Thomas Ari Bech <thomasb17@ru.is>
2017-11-30 16:27:32 +0000

    Add SideOrder functions

--- order/SideOrder.h
+++ order/SideOrder.h
@@ -1,8 +1,22 @@
 #pragma once
-class SideOrder
-{
-public:
-	SideOrder();
-	~SideOrder();
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class SideOrder {
+	private:
+		string name;
+		double price;
+	public:
+		SideOrder();
+		SideOrder(string name, double price);
+		~SideOrder();
+		void write(ofstream& fout) const;
+		void read(ifstream& fin);
+		string getName() const;
+		double getPrice() const;
+		friend ostream& operator<< (ostream& out, const SideOrder& side);
+		friend bool operator == (const SideOrder& left, const SideOrder& right);
 };
 
