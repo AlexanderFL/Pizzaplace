3afd3e6f9f22ad923a01b97228c6435ade45058e
Hugrún Hannesdóttir <hugrun17@ru.is>
2017-11-30 17:09:39 +0000

    Salesman addition

--- ui/SalesmanUI.cpp
+++ ui/SalesmanUI.cpp
@@ -6,16 +6,33 @@ SalesmanUI::SalesmanUI()
 }
 
 void SalesmanUI::salesmanMenu() {
-	cout <<"\n\t---------------------------------------------------\n"
-		 << " \t|  Register a pizza: P    |  Add another order: A |\n"
-		 << " \t---------------------------------------------------\n"
-		 << " \t|  Price of the order: K  |  Delivery: D          |\n"
-		 << " \t---------------------------------------------------\n"
-		 << " \t|  Food delivered: L      |  Paid for: S          |\n"
-		 << " \t---------------------------------------------------\n"
-		 << " \t|  Home address: H        |  Comments: C          |\n"
-		 << " \t---------------------------------------------------\n"
-		 << " \t|  Quit : Q               |                       |\n"
-		 << " \t---------------------------------------------------\n" << endl;
+	bool ignore = true;
+	string option;
+	while (true) {
+		cout << "\n\t---------------------------------------------------\n"
+			<< " \t|  Register a pizza: P    |  Add another order: A |\n"
+			<< " \t---------------------------------------------------\n"
+			<< " \t|  Price of the order: K  |  Delivery: D          |\n"
+			<< " \t---------------------------------------------------\n"
+			<< " \t|  Food delivered: L      |  Paid for: S          |\n"
+			<< " \t---------------------------------------------------\n"
+			<< " \t|  Home address: H        |  Comments: C          |\n"
+			<< " \t---------------------------------------------------\n"
+			<< " \t|  Quit : Q               |                       |\n"
+			<< " \t---------------------------------------------------\n" << endl;
+		
+		cout << "What would you like to do? "; 
+		if (ignore) {
+			cin.ignore();
+			ignore = false;
+		}
+		getline(cin, option);
+		//so that it doesn't matter if the string is all caps or not
+		transform(option.begin(), option.end(), option.begin(), ::tolower);
+		if (option == "q") {
+			cout << "gs";
+			break;
+		}
+	}
 
 }
--- ui/SalesmanUI.h
+++ ui/SalesmanUI.h
@@ -1,6 +1,6 @@
-#pragma once
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class SalesmanUI
