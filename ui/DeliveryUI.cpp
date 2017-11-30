cd7097fb820a17744a88e60f9241e02675b5fbf7
Hugrún Hannesdóttir <hugrun@art.is>
2017-11-30 15:00:04 +0000

    DeliveryUI

--- ui/DeliveryUI.cpp
+++ ui/DeliveryUI.cpp
@@ -9,3 +9,30 @@ DeliveryUI::DeliveryUI()
 DeliveryUI::~DeliveryUI()
 {
 }
+
+void DeliveryUI::deliveryMenu() {
+	string homeAddress;
+	int numberOfOrders = 0;
+	char input;
+	while (true) {
+		cout << "Where would you like it delivered? ";
+		getline(cin, homeAddress);
+		cout << "Your home address is " << homeAddress << ". Is this correct? (y/n): ";
+		cin >> input;
+		if (tolower(input) == 'y') {
+			break;
+		}
+		//To be able to use getline again without error
+		cin.clear();
+		cin.sync();
+	}
+	if (numberOfOrders > 1) {
+		cout << "ba;";
+	}
+	else if (numberOfOrders == 1) {
+		cout << "d3";
+	}
+	else {
+		cout << "hsdg";
+	}
+}
