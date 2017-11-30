0386f3084ddaf9da4c9eea6a1da443c60ca583de
Thomas Ari Bech <thomasb17@ru.is>
2017-11-30 16:43:32 +0000

    Old string binary write

--- pizza/Topping.cpp
+++ pizza/Topping.cpp
@@ -14,9 +14,15 @@ vector<Topping> Topping::getAllToppings()
 }
 
 void Topping::write(ofstream& fout) const {
-	int len = this->name.length() + 1;
+	//int len = this->name.length() + 1;
+	int len = this->name.length();
 	fout.write((char*)(&len), sizeof(int));
-	fout.write(this->name.c_str(), len);
+	char n;
+	for (int i = 0; i < len; ++i) {
+		n = this->name[i];
+		fout.write(&n, sizeof(char));
+	}
+	//fout.write(this->name.c_str(), len);
 	fout.write((char*)(&this->price), sizeof(double));
 }
 
@@ -24,11 +30,16 @@ void Topping::read(ifstream& fin) {
 	int len;
 	this->name = "";
 	fin.read((char*)(&len), sizeof(int));
-	char* str = new char[len];
-	fin.read(str, len);
-	this->name = str;
+	//char* str = new char[len];
+	//fin.read(str, len);
+	char n;
+	for (int i = 0; i < len; ++i) {
+		fin.read(&n, sizeof(char));
+		this->name += n;
+	}
+	//this->name = str;
 	fin.read((char*)(&this->price), sizeof(double));
-	delete[] str;
+	//delete[] str;
 }
 
 double Topping::getPrice() {
