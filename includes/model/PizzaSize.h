5cbddbb2b95c0fdb623712e09199c36c4fb779ea
Thomas Ari Bech <thomasb17@ru.is>
2017-12-06 10:05:10 +0000

    Add PizzaSize class

---
+++ includes/model/PizzaSize.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class PizzaSize
+{
+private:
+	string name;
+	double pricemod;
+public:
+	PizzaSize();
+	PizzaSize(string name, double pricemod);
+	string getName() const;
+	double getPriceMod() const;
+	void setName(const string& name);
+	void setPriceMod(const double& pricemod);
+	friend ostream& operator << (ostream& out, const PizzaSize& size);
+	friend istream& operator >> (istream& in, PizzaSize& size);
+};
+
---
+++ src/model/PizzaSize.cpp
@@ -0,0 +1,51 @@
+#include "PizzaSize.h"
+#include <fstream>
+
+
+PizzaSize::PizzaSize() {
+	this->pricemod = 1;
+}
+
+PizzaSize::PizzaSize(string name, double pricemod) {
+	this->name = name;
+	this->pricemod = pricemod;
+}
+
+string PizzaSize::getName() const {
+	return this->name;
+}
+
+double PizzaSize::getPriceMod() const {
+	return this->pricemod;
+}
+
+void PizzaSize::setName(const string& name) {
+	this->name = name;
+}
+
+void PizzaSize::setPriceMod(const double& pricemod) {
+	this->pricemod = pricemod;
+}
+
+ostream& operator << (ostream& out, const PizzaSize& size) {
+	if (&out != &cout) {
+		int len = size.name.length() + 1;
+		out.write((char*)(&len), sizeof(int));
+		out.write(size.name.c_str(), len);
+		out.write((char*)(&size.pricemod), sizeof(double));
+	}
+	return out;
+}
+
+istream& operator >> (istream& in, PizzaSize& size) {
+	if (&in != &cin) {
+		int len;
+		in.read((char*)(&len), sizeof(int));
+		char* str = new char[len];
+		in.read(str, len);
+		size.name = str;
+		delete[] str;
+		in.read((char*)(&size.pricemod), sizeof(double));
+	}
+	return in;
+}
