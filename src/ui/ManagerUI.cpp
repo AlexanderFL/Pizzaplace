33b2082e81384f0dcb8e79c11813f02c0c5c3673
Hugrún Hannesdóttir <hugrun17@ru.is>
2017-12-06 12:04:21 +0000

    making it more clean

--- src/ui/ManagerUI.cpp
+++ src/ui/ManagerUI.cpp
@@ -7,8 +7,11 @@
 //For clearing the screen
 #include <stdlib.h>
 
-void ManagerUI::managerMenu() {
+ManagerUI::ManagerUI() {
 	char input = ' ';
+}
+
+void ManagerUI::managerMenu() {
 	bool ignore = true;
 	Menu menu;
 	while (true) {
@@ -30,9 +33,12 @@ void ManagerUI::managerMenu() {
 		}
 		case '2':
 		{
-			toppings();
+			cout << menu.printMenu({ "Pizza", "Toppings", "Price", "Delivery", "Other", "Go Back" }) << endl;
+			cout << "Input: ";
+			cin >> input;
+			validateToppingInput(input);
 			break;
-		}
+			}
 		case '3':
 		{
 			//TODO: get the total price of an order
@@ -57,61 +63,64 @@ void ManagerUI::managerMenu() {
 	}
 }
 
+void ManagerUI::validateToppingInput(char input) {
+	switch (input) {
+	case '1':
+		addTopping();
+		break;
+	case '2':
+		deleteTopping();
+		break;
+	case '3': {
+		Data toppingRepo;
+		Topping topping;
+		vector <Topping> toppings = toppingRepo.RetrieveAllFromFile<Topping>();
+		cout << "Here are the toppings you have so far: " << endl;
+		for (unsigned int i = 0; i < toppings.size(); i++) {
+			cout << i + 1 << ": " << toppings.at(i) << endl;
+		}
+		break;
+	}
+	default:
+		cout << "Invalid input." << endl;
+	}
+}
 
-//TODO: FIX
-void ManagerUI::toppings() {
+void ManagerUI::addTopping() {
 	int numberOfToppings;
 	Data toppingRepo;
 	Topping topping;
-	char input;
-	cout << "What would you like to do? \n"
-		<< "1. Add a topping.\n"
-		<< "2. Delete a topping.\n"
-		<< "3. See all the toppings you have so far.\n"
-		<< "Input: ";
+	//TODO: fix and add (also be able to delete a toping)
+	cout << "How many toppings would you like to add? ";
+	cin >> numberOfToppings;
+	cout << "What would you like as a topping? " << endl;
+
+	for (int i = 0; i < numberOfToppings; ++i) {
+		cout << "Topping number " << i + 1 << ": ";
+		cin >> topping;
+		toppingRepo.WriteToFile(topping);
+	}
+}
+
+void ManagerUI::deleteTopping() {
+	Data toppingRepo;
+	Topping topping;
+	vector <Topping> toppings = toppingRepo.RetrieveAllFromFile<Topping>();
+	cout << "Here are the toppings you have so far: " << endl;
+	for (unsigned int i = 0; i < toppings.size(); i++) {
+		cout << i + 1 << ": " << toppings.at(i) << endl;
+	}
+	cout << "What topping would you like to delete. Please input a number or 'q' to quit." << endl;
+	cout << "Input: ";
 	cin >> input;
-	//Add a topping
-	if (input == '1') {
-		//TODO: fix and add (also be able to delete a toping)
-		cout << "How many toppings would you like to add? ";
-		cin >> numberOfToppings;
-		cout << "What would you like as a topping? " << endl;
+	//Changing the input from char to int
+	int inputInInt = (int)input - 49;
+	if (tolower(input) == 'q') {
+		exit(1);
+	}
 
-		for (int i = 0; i < numberOfToppings; ++i) {
-			cout << "Topping number " << i + 1 << ": ";
-			cin >> topping;
-			toppingRepo.WriteToFile(topping);
-		}
-	}
-	//Delete a topping
-	else if (input == '2') {
-		vector <Topping> toppings = toppingRepo.RetrieveAllFromFile<Topping>();
-		cout << "Here are the toppings you have so far: " << endl;
-		for (unsigned int i = 0; i < toppings.size(); i++) {
-			cout << i + 1 << ": " << toppings.at(i) << endl;
-		}
-		cout << "What topping would you like to delete. Please input a number or 'q' to quit." << endl;
-		cout << "Input: ";
-		cin >> input;
-		//Changing the input from char to int
-		int inputInInt = (int)input - 49;
-		if (tolower(input) == 'q') {
-			exit(1);
-		}
-
-		else if (inputInInt < 0 || toppings.size() <= inputInInt) {
-			cout << "Invalid input." << endl;
-		}
-		else toppingRepo.RemoveFromFileAtIndex<Topping>(inputInInt);
-	}
-	else if (input == '3') {
-		vector <Topping> toppings = toppingRepo.RetrieveAllFromFile<Topping>();
-		cout << "Here are the toppings you have so far: " << endl;
-		for (unsigned int i = 0; i < toppings.size(); i++) {
-			cout << i + 1 << ": " << toppings.at(i) << endl;
-		}
-	}
-	else {
+	else if (inputInInt < 0 || toppings.size() <= inputInInt) {
 		cout << "Invalid input." << endl;
 	}
+	else toppingRepo.RemoveFromFileAtIndex<Topping>(inputInInt);
 }
