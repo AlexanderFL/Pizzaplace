74d98a5f1c86c87bbaae24100cf7c39b4d075862
Thomas Ari Bech <thomasb17@ru.is>
2017-12-06 12:40:59 +0000

    Add User functions

--- includes/model/User.h
+++ includes/model/User.h
@@ -5,12 +5,22 @@ using namespace std;
 
 class User
 {
-private:
-	string name;
-	int level;
 public:
-	User();
-	User(int level);
-	User(string name, int level);
+	static const string filename;
+private:
+	string username;
+	string password;
+	string name;
+	int job;
+public:
+	User(string username, string password, string name, int job);
+	string getUsername() const;
+	string getPassword() const;
+	string getName() const;
+	int getJob() const;
+	void setUsername(const string& username);
+	void setPassword(const string& password);
+	void setName(const string& name);
+	void setJob(const int& job);
 };
 
--- src/model/User.cpp
+++ src/model/User.cpp
@@ -1,14 +1,43 @@
 #include "User.h"
 
-User::User(string name, int level)
-{
+const string PizzaCrust::filename = "users.dat";
+
+User::User(string username, string password, string name, int job) {
+	this->username = username;
+	this->password = password;
 	this->name = name;
-	this->level = level;
+	this->job = job;
 }
 
-User::User(int level)
-{
-	this->level = level;
+string getUsername() const {
+	return this->username;
 }
 
-User::User() { }
+string getPassword() const {
+	return this->password;
+}
+
+string getName() const {
+	return this->name;
+}
+
+int getJob() const {
+	return this->job;
+}
+
+void setUsername(const string& username) {
+	this->username = username;
+}
+
+void setPassword(const string& password) {
+	this->password = password;
+}
+
+void setName(const string& name) {
+	this->name = name;
+}
+
+void setJob(const int& job) {
+	this->job = job;
+}
+
