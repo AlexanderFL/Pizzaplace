20ae41144144c845ddddef18cdd561e7b6a63723
Thomas Ari Bech <thomasb17@ru.is>
2017-12-06 09:36:52 +0000

    Add Location class

---
+++ includes/model/Location.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Location
+{
+private:
+	string name;
+public:
+	Location();
+	Location(string name);
+	string getName() const;
+	void setName(const string& name);
+	friend ostream& operator << (ostream& out, const Location& loc);
+	friend istream& operator >> (istream& in, Location& loc);
+};
+
---
+++ src/model/Location.cpp
@@ -0,0 +1,38 @@
+#include "Location.h"
+#include <fstream>
+
+
+Location::Location() {}
+
+Location::Location(string name) {
+	this->name = name;
+}
+
+string getName() const {
+	return this->name;
+}
+
+void setName(const string& name) {
+	this->name = name;
+}
+
+ostream& operator << (ostream& out, const Location& loc) {
+	if (&out != &cout) {
+		int len = loc.name.length() + 1;
+		out.write((char*)(&len), sizeof(int));
+		out.write(loc.name.c_str(), len);
+	}
+	return out;
+}
+
+istream& operator >> (istream& in, Location& loc) {
+	if (&in != &cin) {
+		int len;
+		in.read((char*)(&len), sizeof(int));
+		char* str = new char[len];
+		in.read(str, len);
+		loc.name = str;
+		delete[] str;
+	}
+	return in;
+}
