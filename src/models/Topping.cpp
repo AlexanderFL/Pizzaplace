cfe32aed8c9a9a88a7b216211dbc4ffa7922b928
Thomas Ari Bech <thomasb17@ru.is>
2017-12-09 19:14:33 +0000

    Add id to Topping

--- src/models/Topping.cpp
+++ src/models/Topping.cpp
@@ -21,12 +21,20 @@ int Topping::getPrice() const{
 	return this->price;
 }
 
+size_t Topping::getID() const {
+	return this->id;
+}
+
 void Topping::setName(const string& name) {
 	this->name = name;
 }
 
 void Topping::setPrice(const int& price) {
 	this->price = price;
+}
+
+void Topping::setID(const size_t& id) {
+	this->id = id;
 }
 
 istream& operator >> (istream& in, Topping& topping) {
@@ -38,6 +46,7 @@ istream& operator >> (istream& in, Topping& topping) {
 		topping.name = str;
 		in.read((char*)(&topping.price), sizeof(int));
 		delete[] str;
+		in.read((char*)(&topping.id), sizeof(size_t));
 	}
 	else {
 		cout << "\nName: ";
@@ -55,6 +64,7 @@ ostream& operator<< (ostream& out, const Topping& topping)
 		out.write((char*)(&len), sizeof(size_t));
 		out.write(topping.name.c_str(), len);
 		out.write((char*)(&topping.price), sizeof(int));
+		out.write((char*)(&topping.id), sizeof(size_t));
 	}
 	else {
 		out << topping.name << " " << topping.price;
