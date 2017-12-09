3665d7db9c11871c675264acd1066d149116a675
Thomas Ari Bech <thomasb17@ru.is>
2017-12-09 18:25:46 +0000

    Add id to Offer

--- includes/models/Offer.h
+++ includes/models/Offer.h
@@ -15,6 +15,7 @@ private:
 	Order order;
 	int price;
 	bool fixed;
+	size_t id;
 public:
 	// Constructors
 	Offer();
@@ -25,12 +26,14 @@ public:
 	Order getOrder() const;
 	int getPrice() const;
 	bool getFixed() const;
+	size_t getID() const;
 
 	// Set functions
 	void setName(const string& name);
 	void setOrder(const Order& order);
 	void setPrice(const int& price);
 	void setFixed(const bool& fixed);
+	void setID(const size_t& id);
 
 	// Operator overloads
 	friend ostream& operator << (ostream& out, const Offer& offer);
