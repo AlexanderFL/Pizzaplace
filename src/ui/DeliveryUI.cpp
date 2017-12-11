d1f070ff71ed63e4b5de463c480c206366b2593b
Thomas Ari Bech <thomasb17@ru.is>
2017-12-11 13:17:43 +0000

    Add func to DeliveryUI

--- src/ui/DeliveryUI.cpp
+++ src/ui/DeliveryUI.cpp
@@ -108,13 +108,13 @@ void DeliveryUI::ordersMenu() {
 		try {
 			vector<Order> orders = service.getOrders(locations.at((int)input - 49));
 			for (size_t i = 0; i < orders.size(); ++i) {
-				cout << i + 1 << ": " << orders.at(i).getStatus() << endl;
+				cout << i + 1 << ": " << showOrderInfoShort(orders.at(i)) << endl;
 			}
 			cout << "Input: ";
 			cin >> input;
 			try {
 				int order = (int)input - 49;
-				cout << orders.at(order) << endl;
+				cout << showOrderInfo(orders.at(order)) << endl;
 				cout << "Set as paid" << endl;
 				cout << "Set as delivered" << endl;
 				cout << "Go back" << endl;
@@ -166,4 +166,78 @@ void DeliveryUI::ordersMenu() {
 	catch (FailedOpenFile) {
 		cout << "Failed to open file." << endl;
 	}
+}
+
+string DeliveryUI::showPizzaInfoShort(const Pizza& pizza) const {
+	string info = "";
+	info += pizza.getID() + " - ";
+	info += pizza.getToppings().size() + " toppings, ";
+	info += pizza.getPizzaSize().getName() + " ";
+	info += pizza.getCrust().getName() + " pizza - ";
+	if (pizza.getPhase() == PREPERATION) {
+		info += "in preperation";
+	}
+	else if (pizza.getPhase() == BAKING) {
+		info += "baking";
+	}
+	return info;
+}
+
+string DeliveryUI::showOrderInfo(const Order& order) const {
+	string info = "Order: " + order.getID();
+	info += "Pizzas: ";
+	if (order.getPizzas().size() == 0) {
+		info += "None";
+	}
+	else {
+		for (size_t i = 0; i < order.getPizzas().size(); ++i) {
+			info += showPizzaInfoShort(order.getPizzas().at(i));
+		}
+	}
+	info += "Side Orders: ";
+	if (order.getSides().size() == 0) {
+		info += "None";
+	}
+	else {
+		for (size_t i = 0; i < order.getSides().size(); ++i) {
+			info += order.getSides().at(i).getName();
+		}
+	}
+	info += "Comment: " + order.getComment();
+	info += "Location: " + order.getLocation().getAddress();
+	info += "Delivery Method: ";
+	if (order.getDeliveryMethod() == PICKUP) {
+		info += "Pickup";
+	}
+	else if (order.getDeliveryMethod() == SEND) {
+		info += "Send to " + order.getHomeAddress();
+	}
+	info += "Paid: ";
+	if (order.isPaidFor()) {
+		info += "True";
+	}
+	else {
+		info += "False";
+	}
+	return info;
+}
+
+string DeliveryUI::showOrderInfoShort(const Order& order) const {
+	string info = "";
+	info += order.getID() + " - ";
+	info += order.getPizzas().size() + " pizzas, ";
+	info += order.getSides().size() + " side orders, ";
+	if (order.isPaidFor()) {
+		info += " paid, ";
+	}
+	else {
+		info += " unpaid, ";
+	}
+	if (order.getDeliveryMethod() == SEND) {
+		info += " needs to be sent";
+	}
+	else {
+		info += " needs to be picked up";
+	}
+	return info;
 }
