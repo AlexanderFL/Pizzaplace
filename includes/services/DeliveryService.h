691f37fd403b810e812f5eaaa90888447a208558
Thomas Ari Bech <thomasb17@ru.is>
2017-12-15 03:19:52 +0000

    Add DeliveryService.h comments

--- includes/services/DeliveryService.h
+++ includes/services/DeliveryService.h
@@ -9,8 +9,12 @@ private:
 public:
 	DeliveryService();
 
+	//Gets all orders that are in ready and need to be paid and delivered 
 	vector<Order> getOrders();
+	//Gets all orders that are in ready and need to be paid and delivered for a location
 	vector<Order> getOrders(const Location& location);
+	//Sets the order of the ID as paid
 	void setOrderPaid(const size_t& id);
+	//Sets the order of the ID as delivered
 	void setOrderDelivered(const size_t& id);
 };
